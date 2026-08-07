4cd77b1a303db20f8c0e108354749a264d4b7223
memetreza34-ux <memetreza34@gmail.com>
2026-08-07 17:43:12 +0200

    feat: mount global error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import ErrorBoundary from './ErrorBoundary.jsx'
 import RootApp from './RootApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RootApp />
+    <ErrorBoundary>
+      <RootApp />
+    </ErrorBoundary>
   </StrictMode>,
 )
