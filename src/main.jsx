d048fe2f0c49c97bb89b8009ad4ac0b5288c9bcb
memetreza34-ux <memetreza34@gmail.com>
2026-08-28 10:41:10 +0200

    Normalize local history before React startup

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import ErrorBoundary from './ErrorBoundary.jsx'
 import RootApp from './RootApp.jsx'
+import { normalizeLocalHistoryStores } from './localDataBootstrap.js'
+
+normalizeLocalHistoryStores()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
